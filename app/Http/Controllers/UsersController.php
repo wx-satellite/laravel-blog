d0a0ed0a3ceeec725a77c4e4f2a08751a0c3b00a
wxsatellite <1453085314@qq.com>
2019-09-08 15:44:50 +0000

    增加csrf

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -16,4 +16,14 @@ class UsersController extends Controller
     public function show(User $user) {
         return view("users.show", compact('user'));
     }
+
+    public function store(Request $request) {
+        $this->validate($request,[
+            "name" => "required|max:50",
+            "email" => "required|email|unique:users|max:255",
+            // confirmed验证两次密码是否一致，表单的确认密码字段必须是"password_confirmation"
+            "password" => "required|confirmed"
+        ]);
+        return ;
+    }
 }
