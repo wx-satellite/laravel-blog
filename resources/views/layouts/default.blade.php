6d9bb2072264492d00210f6de0d3583fb5d6fb3e
wxsatellite <1453085314@qq.com>
2019-09-08 05:39:20 +0000

    美化样式

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -1,9 +1,23 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>@yield("title","Wx-Blog")</title>
+    <title>@yield('title', 'Wx-Blog') - Laravel 入门教程</title>
+    <link rel="stylesheet" href="{{mix('css/app.css')}}">
 </head>
 <body>
-@yield("content")
+
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div class="container">
+        <a class="navbar-brand" href="/">Wx-Blog</a>
+        <ul class="navbar-nav justify-content-end">
+            <li class="nav-item"><a class="nav-link" href="/help">帮助</a></li>
+            <li class="nav-item" ><a class="nav-link" href="#">登录</a></li>
+        </ul>
+    </div>
+</nav>
+
+<div class="container">
+    @yield('content')
+</div>
 </body>
 </html>
